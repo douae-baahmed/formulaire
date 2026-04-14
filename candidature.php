41efab753aaa05075466e282d73546d0b8c0777b
Douae Baahmed <d.baahmed1.9@gmail.com>
2026-04-14 15:06:32 +0100

    feat: persistance des valeurs apres soumission

--- candidature.php
+++ candidature.php
@@ -59,37 +59,37 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <form  method="post">
 
 <label>prenom :
-    <input type="text" name="prenom" >
+    <input type="text" name="prenom" value="<?php echo $prenom; ?>">
 </label>
 
 <label>nom :
-    <input type="text" name="nom" >
+    <input type="text" name="nom" value="<?php echo $nom; ?>">
 </label>
 
 <label> email :
-    <input type="email" name="email" >
+    <input type="email" name="email" value="<?php echo $email; ?>">
 </label>
 
 <label>age :
-    <input type="number" name="age" >
+    <input type="number" name="age" value="<?php echo $age; ?>">
 </label>
 
 <label> filiere :
     <select name="filiere">
         <option value=""></option>
-        <option value="info" >Informatique</option>
-        <option value="elec">Électronique</option>
-        <option value="meca" >Mécanique</option>
-        <option value="autre" >Autre</option>
+        <option value="info" <?php echo ($filiere === 'info') ? 'selected' : ''; ?>>Informatique</option>
+        <option value="elec" <?php echo ($filiere === 'elec') ? 'selected' : ''; ?>>Électronique</option>
+        <option value="meca" <?php echo ($filiere === 'meca') ? 'selected' : ''; ?>>Mécanique</option>
+        <option value="autre" <?php echo ($filiere === 'autre') ? 'selected' : ''; ?>>Autre</option>
     </select>
 </label>
 
 <label>lettre de motivation :
-    <textarea name="motivation" rows="6"></textarea>
+    <textarea name="motivation" rows="6"><?php echo $motivation; ?></textarea>
 </label>
 
 <label>
-    <input type="checkbox" name="reglement" value="1" >
+    <input type="checkbox" name="reglement" value="1" <?php echo $reglement ? 'checked' : ''; ?>>
     J'ai lu et j'accepte le règlement du club.
 </label>
 
